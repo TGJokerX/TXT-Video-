eba9bfbecc3f8902081c1027c9cf8fbcf1087bb3
rohittxtbotz <rohittxtbots@gmail.com>
2025-01-06 16:07:46 +0530

    Update and rename config (1).py to config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ class Config(object):
     # get a token from @BotFather
     pass
     """
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7124482349:AAEsgY0dfcdCmVRs39THUSNizGJCahjwpQE")
     API_ID = int(os.environ["API_ID", 24935975]
     API_HASH = os.environ["API_HASH", "7807918602:AAH9U4aFxkU-kb2r09ub4L42IobR417xbRs"]
     AUTH_USERS = "6750546542"""
